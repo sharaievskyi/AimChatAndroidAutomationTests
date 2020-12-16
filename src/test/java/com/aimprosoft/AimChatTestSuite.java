17e3043de37ad8cb67687d780a6e1f880008acb9
sharaievskyi <d.sharaievskyi@aimprosoft.com>
2020-12-16 18:32:35 +0200

    16.12.20

--- src/test/java/com/aimprosoft/AimChatTestSuite.java
+++ src/test/java/com/aimprosoft/AimChatTestSuite.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features= {
         "src/test/resources/features/regression_testing/001_Authorization.feature",
         "src/test/resources/features/regression_testing/002_LeftDrawer.feature",
-        "src/test/resources/features/regression_testing/003_MessageHistory.feature",
-        "src/test/resources/features/regression_testing/004_RightDrawer.feature",
-        "src/test/resources/features/regression_testing/005_Toolbar.feature",
-        "src/test/resources/features/regression_testing/006_MessageBar.feature"
+//        "src/test/resources/features/regression_testing/003_MessageHistory.feature",
+//        "src/test/resources/features/regression_testing/004_RightDrawer.feature",
+//        "src/test/resources/features/regression_testing/005_Toolbar.feature",
+//        "src/test/resources/features/regression_testing/006_MessageBar.feature"
 })
 public class AimChatTestSuite {}
